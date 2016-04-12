6030d971e56c6ce8fcc03924da0149cb60250d1d
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-12 15:00:22 -0500

    create venues table with migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.string "artist"
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
   end
 
 end
