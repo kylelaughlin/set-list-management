2eaa43f733e327835ffb976c40ee083108adb715
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-13 10:40:12 -0500

    add basic styling to initial new setlist view

--- app/controllers/set_list_contollers.rb
+++ app/controllers/set_list_contollers.rb
@@ -3,3 +3,9 @@ get '/set_lists/new' do
   @SetList = SetList.new
   erb :"set_lists/new"
 end
+
+get '/set_lists/new/songs' do
+  @sets = params['sets']
+  @num_of_songs = params['songs']
+  binding.pry
+end
