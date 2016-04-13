ff1d1eb2d1885bd6135f797b4f0f7e5141e83074
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-13 16:26:42 -0500

    add documentation to explain use of key value pairs in the params hash

--- app/models/set_list.rb
+++ app/models/set_list.rb
@@ -11,6 +11,13 @@ class SetList < ActiveRecord::Base
   has_many :songs, through: :set_items
   belongs_to :venue
 
+  # Creates SetItem records according to the key value pairs of the params hash
+  #
+  # +params: a hash representing key value pairs from the set list form
+  # =>        key is structured "1,3" which represents the third song of the first set
+  # =>        value is the id of the associated song records
+  #
+  # Returns nil - writes to the database
   def create_set_item(params, set_list_id)
     binding.pry
     (1..4).each do |set|
