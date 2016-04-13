1abc4eda1f68cc15b2205507fc05431db2b01dd5
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-13 11:32:34 -0500

    create initial view for basic set list information

--- app/controllers/set_list_contollers.rb
+++ app/controllers/set_list_contollers.rb
@@ -1,11 +1,25 @@
 
 get '/set_lists/new' do
-  @SetList = SetList.new
+  @set_list = SetList.new
   erb :"set_lists/new"
 end
 
-get '/set_lists/new/songs' do
+post '/set_lists/new/list' do
+  binding.pry
+  @name = params['name']
+  #break this into a helper class?
+  date_parts = params['date'].split("-")
+  @date = Date.new(date_parts[0].to_i,date_parts[1].to_i,date_parts[2].to_i)
+  @set_list = SetList.new(name: @name, performance_date: @date)
+  if @set_list.save
+    redirect to("/set_lists/new/#{@set_list.id}")
+  else
+    erb :"set_lists/new"
+  end
+end
+
+get '/set_lists/new/:id' do
   @sets = params['sets']
   @num_of_songs = params['songs']
-  binding.pry
+  erb :"set_lists/new_list"
 end
