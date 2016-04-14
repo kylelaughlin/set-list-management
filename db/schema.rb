6acb7ecfa69f94c32f6d645b022c5dc8641b1b63
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-14 14:37:34 -0500

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 7) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string "title"
-    t.string "artist"
+    t.string  "title"
+    t.string  "artist"
+    t.boolean "active", default: true
   end
 
   create_table "venues", force: :cascade do |t|
