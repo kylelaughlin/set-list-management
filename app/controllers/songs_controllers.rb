73877c19a5cd60b0ff92670ba5e4a3ae33c88664
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-14 14:15:05 -0500

    edit song and band members

--- app/controllers/songs_controllers.rb
+++ app/controllers/songs_controllers.rb
@@ -13,5 +13,19 @@ end
 
 get '/songs/:id/edit' do
   @song = Song.find_by_id(params['id'])
+  @band_members = BandMember.all
+  @song_players = @song.band_members
   erb :"songs/edit"
+end
+
+patch '/songs/:id' do
+  @song = Song.find_by_id(params['id'])
+  @song.update_band_members(params)
+  @band_members = BandMember.all
+  @song_players = @song.band_members
+  if @song.update_attributes(title: params['title'], artist: params['artist'])
+    redirect to("/songs/#{@song.id}")
+  else
+    erb :"songs/edit"
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -8,5 +8,32 @@ class Song < ActiveRecord::Base
 
   has_and_belongs_to_many :band_members
   has_many :set_items
-  has_many :set_lists, through: :set_items 
+  has_many :set_lists, through: :set_items
+
+=begin
+  def update_band_members(params)
+    params.each do |k,v|
+      if (k =~ /band/) == 0
+        band_member = BandMember.find_by_id(v)
+        self.band_members << band_member
+      end
+    end
+  end
+=end
+
+  def update_band_members(params)
+    self.band_members.each do |member|
+      if !params['band_members'].include?(member)
+        self.band_members.destroy(member)
+      end
+    end
+    params['band_members'].each do |id|
+      member = BandMember.find_by_id(id.to_i)
+      if !self.band_members.include?(member)
+
+        self.band_members << member
+      end
+    end
+  end
+
 end
