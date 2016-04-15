8f57bd6b27a7add895ccd1e69555b3178ad2af74
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-15 09:02:36 -0500

    Create new view and route for venues

--- app/controllers/venues_controllers.rb
+++ app/controllers/venues_controllers.rb
@@ -8,3 +8,12 @@ get '/venues/new' do
   @venue = Venue.new
   erb :"venues/new"
 end
+
+post '/venues' do
+  @venue = Venue.new(name: params['name'], address: params['address'])
+  if @venue.save
+    redirect to("/venues")
+  else
+    erb :"venues/new"
+  end
+end
