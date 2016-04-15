5ea2b2b53e3b66f38852d68110d51e702bd1ac7f
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-14 22:00:33 -0500

    Create band member show view and edit view and route

--- app/controllers/band_members_controllers.rb
+++ app/controllers/band_members_controllers.rb
@@ -2,3 +2,22 @@ get "/band_members" do
   @band_members = BandMember.all
   erb :"band_members/index"
 end
+
+get "/band_members/:id" do
+  @band_member = BandMember.find_by_id(params['id'])
+  erb :"band_members/show"
+end
+
+get "/band_members/:id/edit" do
+  @band_member = BandMember.find_by_id(params['id'])
+  erb :"band_members/edit"
+end
+
+patch "/band_members/:id" do
+  @band_member = BandMember.find_by_id(params['id'])
+  if @band_member.update_attributes(name: params['name'], instrument: params['instrument'])
+    redirect to("/band_members/#{@band_member.id}")
+  else
+    erb :"band_members/edit"
+  end
+end
