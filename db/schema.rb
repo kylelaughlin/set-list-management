091a03f9557559b0841d1f2a02fd0395f24ba6eb
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-18 10:46:15 -0500

    remove venue table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 10) do
+ActiveRecord::Schema.define(version: 11) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,11 +46,6 @@ ActiveRecord::Schema.define(version: 10) do
     t.string  "title"
     t.string  "artist"
     t.boolean "active", default: true
-  end
-
-  create_table "venues", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
   end
 
 end
